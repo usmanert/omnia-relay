99903551258e8ff0c370f2878727279d1e76fe64
abbasidd <usman.bsit122@iiu.edu.pk>
2023-05-09 14:49:43 +0500

    update scripts

--- setup/run_it_relay.sh
+++ setup/run_it_relay.sh
@@ -4,6 +4,7 @@ do
 case "$1" in
 --gofer)
 cp -v $2 $(pwd)/result/share/gofer.json
+cp -v $2 $(pwd)/result/share/gofer_temp.json
 export GOFER_CONFIG=$2
 # The result-3 directory is created by nix and it is a symbolic link of nix's installed package, which contain spire , gofer, ghost, keeman etc
 
@@ -11,7 +12,8 @@ export GOFER_PATH=$(pwd)/result-3/bin/gofer
 shift
   ;;
 --omnia) 
-sudo cp $2 $(pwd)/result/share/relay--relay.json 
+sudo cp $2 $(pwd)/result/share/relay-ethereum-relay.json
+sudo cp $2 $(pwd)/result/share/relay-ethereum-relay-temp.json 
 export OMNIA_CONFIG=$2
 export OMNIA_VERBOSE=true
 # The result-2 directory is created by nix and it is a symbolic link of nix's installed package, which contain omnia
@@ -19,7 +21,8 @@ export OMNIA_PATH=$(pwd)/result-2/bin/omnia
 shift
 ;;
 --spire) 
-cp -v $2 $(pwd)/result/share/spire.json
+sudo cp -v $2 $(pwd)/result/share/spire.json
+sudo cp -v $2 $(pwd)/result/share/spire-temp.json
 export SPIRE_CONFIG=$2
 # The result-3 directory is created by nix and it is a symbolic link of nix's installed package, which contain spire , gofer, ghost, keeman etc
 export SPIRE_PATH=$(pwd)/result-3/bin/spire
@@ -35,3 +38,7 @@ done
 
 export SETZER_MIN_MEDIAN=1
 $(pwd)/result/bin/install-relay relay
+sudo cp -v  $(pwd)/result/share/spire-temp.json $SPIRE_CONFIG
+sudo cp -v  $(pwd)/result/share/relay-ethereum-relay-temp.json $OMNIA_CONFIG
+sudo cp -v  $(pwd)/result/share/gofer_temp.json $GOFER_CONFIG
+
